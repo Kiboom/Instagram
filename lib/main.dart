1e47b7898a3f2f3ec716c258c924363582c589c4
Kiboom <kiboomy@gmail.com>
2026-05-19 10:11:19 +0900

    stash

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram/firebase_options.dart';
 import 'package:instagram/pages/login_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const InstagramApp());
 }
 
@@ -13,13 +18,15 @@ class InstagramApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // FirebaseAuth로부터 현재 로그인한 사용자 정보를 가져옵니다.
 
-    // 만약 사용자가 로그인하지 않은 상태라면 `LoginPage`를 보여줍니다.
-    // 만약 사용자가 로그인한 상태라면 `FeedPage`를 보여줍니다.
+    // 만약 사용자가 로그인하지 않은 상태라면 `로그인 페이지`를 보여줍니다.
+    // 만약 사용자가 로그인한 상태라면 `홈 페이지`를 보여줍니다.
 
     return MaterialApp(
       title: 'Instagram',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.pinkAccent,
+        ),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -1,24 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram/data/post.dart';
+import 'package:instagram/widgets/post_widget.dart';
 
-class FeedPage extends StatelessWidget {
+class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
+
+  @override
+  State<FeedPage> createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  List<Post> _posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadPosts();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF1F2F3),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: [
-            Center(
-              child: Text(
-                '홈 피드 페이지',
+            // 인스타그램 피드 카드
+            for (final item in _posts)
+              PostWidget(
+                item: item,
               ),
-            ),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> loadPosts() async {
+    // FirebaseFirestore로부터 데이터를 받아옵니다.
+    final snapshot = await FirebaseFirestore.instance.collection("posts").orderBy("created_at", descending: true).get();
+    final documents = snapshot.docs;
+
+    // 받아온 데이터를 Post 객체 리스트로 변환합니다.
+    final List<Post> posts = [
+      for (final doc in documents)
+        Post(
+          uid: doc.id,
+          username: doc["username"],
+          description: doc["description"],
+          imageUrl: doc["image_url"],
+          createdAt: doc["created_at"],
+        ),
+    ];
+
+    // Post 객체를 이용하여 화면을 다시 그립니다.
+    setState(() {
+      _posts = posts;
+    });
   }
 }
