15236fedacc75e8fb92ae9d72dac8fc95138a059
Kiboom <kiboomy@gmail.com>
2026-07-27 12:09:29 +0900

    6기 2반 Part1 스타팅 포인트

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -6,6 +6,15 @@ class FeedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("홈 피드"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_right_rounded),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
       backgroundColor: const Color(0xFFF1F2F3),
       body: SafeArea(
         child: Column(
